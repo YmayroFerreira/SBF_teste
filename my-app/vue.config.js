508239e392b7e3598be6797058764be56942b9b6
Ymayrô Ferreira <dev@sscrop.com>
2021-08-26 15:31:49 -0300

    alteração de logo

--- my-app/vue.config.js
+++ my-app/vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    iconPaths: {
+       favicon32: '@/assets/logo_Jordan.svg',
+       favicon32: '@/assets/logo_Jordan.svg',
+      favicon16: '@/assets/logo_Jordan.svg',
+      appleTouchIcon: '@/assets/logo_Jordan.svg',
+      maskIcon: '@/assets/logo_Jordan.svg',
+      msTileImage: '@/assets/logo_Jordan.svg',
+    }
+  }
 }
